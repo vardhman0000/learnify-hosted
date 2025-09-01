0cb625194d806208c039002369d94896b2daa29f
Vardhman Jain <123531456+vardhman0000@users.noreply.github.com>
2025-09-01 07:31:17 +0530

    Update vite.config.js

--- client/vite.config.js
+++ client/vite.config.js
@@ -8,7 +8,7 @@ export default defineConfig({
   server: {
     proxy: {
       '/api': {
-        target: 'http://localhost:4000',
+        target: 'https://online-learning-platform-rdhj.onrender.com',
         changeOrigin: true,
         secure: false,
       },
