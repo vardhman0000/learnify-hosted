8907b3bbd2b7a09ac00a207a15ea45e148e68ca5
Vardhman Jain <123531456+vardhman0000@users.noreply.github.com>
2025-09-01 07:34:57 +0530

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,11 @@ const courseRoutes = require('./src/routes/courseRoutes');
 const lectureRoutes = require('./src/routes/lectureRoutes');
 
 const app = express();
-app.use(cors());
+app.use(cors({
+  origin: "https://learnify-hosted-amber.vercel.app",
+  credentials: true
+}));
+
 app.use(express.json());
 
 app.use('/api/auth', authRoutes);
